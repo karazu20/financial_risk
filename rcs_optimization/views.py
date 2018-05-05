3ff0ef708a5cffa3eb60c91373a733013c969bcd
svg18 <sofiavillers@gmail.com>
2018-05-05 14:36:06 -0500

    se agrega middleware dummy

--- rcs_optimization/views.py
+++ rcs_optimization/views.py
@@ -3,11 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from rcs_optimization.middleware import *
 #from middleware import *
 
 # Create your views here.
+
+
 @login_required
-def main(request):	
-	print 'estoyr en rcs optimization'	
-	contexto={}		
-	return render(request, 'rcs_optimization/main.html', contexto)
+def main(request):
+    print 'estoyr en rcs optimization'
+    execute_rcs()
+    contexto = {}
+    return render(request, 'rcs_optimization/main.html', contexto)
