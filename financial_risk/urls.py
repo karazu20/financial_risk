6ccddffd8830a1b545b5b79f40c02da1fb7cc51f
amartinez <amartinez@ecosoft.com.mx>
2018-09-21 13:42:08 -0500

    fix include url

--- financial_risk/urls.py
+++ financial_risk/urls.py
@@ -13,19 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import include, path
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView, logout_then_login
 from financial_risk.views import Test
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('financial_risk/portal/', include('portal.urls')),
-    path('financial_risk/rcs_contributions/', include('rcs_contributions.urls')),
-    path('financial_risk/rcs_optimization/', include('rcs_optimization.urls')),    
-    path('financial_risk/stress_sensitivity/', include('stress_sensitivity.urls')),    
-    path('', LoginView.as_view(template_name='portal/login.html'), name='login'),
-    path('test/', Test.as_view(), name='test_template'),
-    path('logout/', logout_then_login, name='logout'),  
+    url(r'^admin/', admin.site.urls),
+    url(r'^financial_risk/portal/', include(('portal.urls', 'portal'), namespace='portal')),
+    url(r'^financial_risk/rcs_contributions/', include(('rcs_contributions.urls','rcs_contributions'), namespace='rcs_contributions' )),
+    url(r'^financial_risk/rcs_optimization/', include(('rcs_optimization.urls', 'rcs_optimization'), namespace= 'rcs_optimization')),    
+    url(r'^financial_risk/stress_sensitivity/', include(('stress_sensitivity.urls', 'stress_sensitivity'), namespace= 'stress_sensitivity')),    
+    url(r'^', LoginView.as_view(template_name='portal/login.html'), name='login'),
+    url(r'^test/', Test.as_view(), name='test_template'),
+    url(r'^logout/', logout_then_login, name='logout'),  
+
+
 ]
